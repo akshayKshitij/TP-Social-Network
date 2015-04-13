27e647a5177d5900cb6f7bcc22d994e6cb6ff68e
akshayutture <akshay.utture@gmail.com>
2015-04-13 20:09:22 +0530

    all extra js,css files added. Started working on profile_page

---
+++ main_pages/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<img src="2.jpg" alt="HTML5 Icon" width="128" height="128">
+</body>
+</html>
